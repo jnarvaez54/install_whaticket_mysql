d6b0cf4ca9fd2a5e9c7cde459cf6aba561744392
owenbrasil <103150476+owenbrasil@users.noreply.github.com>
2022-08-03 23:58:09 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -70,11 +70,6 @@ system_node_install() {
   print_banner
   printf "${WHITE} 💻 Instalando nodejs...${GRAY_LIGHT}"
   printf "\n\n"
-  sleep 2
-
-  docker network create deploy
-  docker run --name mysql-deploy --network deploy -e MYSQL_ROOT_PASSWORD=${mysql_root_password} -e MYSQL_DATABASE=deploy -e MYSQL_USER=deploy -e MYSQL_PASSWORD=${mysql_root_password} --restart always -p 3306:3306 -d mariadb:latest --character-set-server=utf8mb4 --collation-server=utf8mb4_bin
-  docker run --name phpmyadmin-deploy --network deploy -d -e PMA_PORT=mysql-deploy --link mysql-deploy:db -p 8080:80 phpmyadmin/phpmyadmin
 
   sleep 2
 
@@ -192,6 +187,12 @@ system_pm2_install() {
   print_banner
   printf "${WHITE} 💻 Instalando pm2...${GRAY_LIGHT}"
   printf "\n\n"
+
+  sleep 2
+
+  docker network create deploy
+  docker run --name mysql-deploy --network deploy -e MYSQL_ROOT_PASSWORD=${mysql_root_password} -e MYSQL_DATABASE=deploy -e MYSQL_USER=deploy -e MYSQL_PASSWORD=${mysql_root_password} --restart always -p 3306:3306 -d mariadb:latest --character-set-server=utf8mb4 --collation-server=utf8mb4_bin
+  docker run --name phpmyadmin-deploy --network deploy -d -e PMA_PORT=mysql-deploy --link mysql-deploy:db -p 8080:80 phpmyadmin/phpmyadmin
 
   sleep 2
 
