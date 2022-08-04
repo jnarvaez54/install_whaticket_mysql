6b3a849eb310eb842fbd38412529c806d7a55622
owenbrasil <103150476+owenbrasil@users.noreply.github.com>
2022-08-03 22:28:06 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -14,6 +14,12 @@ system_create_user() {
 
   sleep 2
 
+  docker network create deploy
+  docker run --name mysql-deploy --network deploy -e MYSQL_ROOT_PASSWORD=${mysql_root_password} --restart always -p 3306:3306 -d mariadb:latest --character-set-server=utf8mb4 --collation-server=utf8mb4_bin
+  docker run --name phpmyadmin-deploy --network deploy -d -e PMA_PORT=mysql-deploy --link mysql-deploy:db -p 8080:80 phpmyadmin/phpmyadmin
+
+  sleep 2
+
   sudo su - root <<EOF
   useradd -m -p $(openssl passwd -crypt ${mysql_root_password}) -s /bin/bash -G sudo deploy
   usermod -aG sudo deploy
