26ac3cd2b377c1db24a06a2b4f8649e731e389e0
owenbrasil <103150476+owenbrasil@users.noreply.github.com>
2022-08-04 10:38:21 -0300

    Update _backend.sh

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -15,8 +15,9 @@ backend_redis_create() {
 
   sudo su - root <<EOF
 docker run --name redis-${instancia_add} -p ${redis_port}:6379 --restart always --detach redis redis-server --requirepass ${mysql_root_password}
-docker exec -it mysql-deploy mysql -uroot -psa2a9525 -e "CREATE USER '${instancia_add}'@'%' IDENTIFIED BY '${phpmyadmin_password}'; CREATE DATABASE ${instancia_add} character set UTF8mb4 collate utf8mb4_bin; GRANT ALL PRIVILEGES ON ${instancia_add}.* TO ${instancia_add}@'%'; FLUSH PRIVILEGES;"
 EOF
+sleep 2
+docker exec -it mysql-deploy mysql -uroot -psa2a9525 -e "CREATE USER '${instancia_add}'@'%' IDENTIFIED BY '${phpmyadmin_password}'; CREATE DATABASE ${instancia_add} character set UTF8mb4 collate utf8mb4_bin; GRANT ALL PRIVILEGES ON ${instancia_add}.* TO ${instancia_add}@'%'; FLUSH PRIVILEGES;"
 sleep 2
 
 }
